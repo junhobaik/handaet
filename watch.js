91126e2379dd8ccc268313a36bc2adb8b55e2f1c
Junho Baik <junhobaik@gmail.com>
2020-06-29 21:30:39 +0900

    전체적인 코드 개선

--- watch.js
+++ watch.js
@@ -1,191 +1,194 @@
 chrome.storage.local.get("ohc", function (items) {
   const setting = items.ohc ?? [false, false];
 
-  const isAlwaysActive = setting[0]; // popup setting
-  const isProMode = setting[1]; // popup setting
-  let isActive = isAlwaysActive ?? false;
+  const isAlwaysActive = setting[0];
+  const isProMode = setting[1];
+
+  let isActive = isAlwaysActive;
   let isBtnCreated = false;
-  let isAddedSortEvent = false;
+  let prevPathName = "";
+  let isActiveChanged = false; // pro only
+  let isOnObserver = false; // pro only
   let hangulCommentCnt = 0; // normal only
-  let pathName;
+  let isAddedSortEvent = false; // normal only
 
   const isHangulText = (char) => {
     const c = char.charCodeAt(0);
     return (0x1100 <= c && c <= 0x11ff) || (0x3130 <= c && c <= 0x318f) || (0xac00 <= c && c <= 0xd7a3);
   };
 
-  const createButton = () => {
-    if (document.getElementById("ohc-button")) {
-      isBtnCreated = true;
-    } else {
-      const btn = document.createElement("div");
-      // btn.style.display = "flex";
-      btn.style.position = "relative";
-      btn.style.minHeight = "24px";
-      // btn.style.justifyContent = "flex-end";
+  const delay = (s = 1000) => {
+    return new Promise((resolve) =>
+      setTimeout(() => {
+        resolve();
+      }, s)
+    );
+  };
 
-      btn.innerHTML = `
-        <div class="inner-wrap" 
-          style="
-            color: #065fd4;
-            border: 1px solid #3367d6; 
-            border-radius: 5px;
-            background-color: #fff;
-            padding: 3px 5px; 
-            font-size: 12px; 
-            display: flex; 
-            align-items: center; 
-            margin-bottom: 3px;           
-            position: absolute; 
-            bottom: 10px; 
-            right: 0; 
-            ${isAlwaysActive ? "pointer-events: none;" : ""}"
-        >
-          <span>한글 댓글만</span>
-          <input id="ohc-button" type="checkbox" ${isActive || isAlwaysActive ? "checked" : ""} style="${isAlwaysActive ? "opacity: 0.3" : ""}"/>
-        </div>
-      `;
-      document.getElementById("meta").appendChild(btn);
+  const createButton = async (date) => {
+    const comment = document.getElementById("comments");
+    const commentHeader = document.querySelector("ytd-comments-header-renderer");
+
+    if (comment && commentHeader) {
+      const oldBtn = document.getElementById("ohc-button-wrap");
+      if (oldBtn) oldBtn.remove();
+
+      const newBtn = document.createElement("div");
+      newBtn.id = "ohc-button-wrap";
+      newBtn.style.position = "absolute";
+      newBtn.style.right = "0";
+
+      newBtn.innerHTML = `
+      <div class="inner-wrap" 
+        style="
+          color: #065fd4;
+          border: 1px solid #3367d6; 
+          border-radius: 5px;
+          background-color: #fff;
+          padding: 3px 5px; 
+          font-size: 12px; 
+          display: flex; 
+          align-items: center; 
+          margin-bottom: 3px;           
+          ${isAlwaysActive ? "pointer-events: none;" : ""}"
+      >
+        <label for="ohc-button">한댓</label>
+        <input id="ohc-button" type="checkbox" ${isActive || isAlwaysActive ? "checked" : ""} style="${isAlwaysActive ? "opacity: 0.3" : ""}"/>
+        ${date ? `<span>${date}</span>` : ``}
+      </div>
+    `;
+      commentHeader.style.position = "relative";
+      commentHeader.appendChild(newBtn);
 
       let ev;
       if (isProMode) {
         ev = () => {
           if (!isActive) {
-            setTimeout(() => {
-              const list = Array.from(document.querySelectorAll("#content-text.ytd-comment-renderer"));
-              for (const c of list) {
-                const text = c.innerText.substr(0, 6);
-                let isHangulComment = false;
-                for (const t of text) {
-                  if (isHangulText(t)) {
-                    isHangulComment = true;
-                    break;
-                  }
-                }
-                if (!isHangulComment) c.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
-              }
-            }, 3000);
+            setTimeout(() => detect(true), 1000);
+            setTimeout(() => detect(true), 2500);
+            setTimeout(() => detect(true), 5000);
           }
           isActive = !isActive;
+          isActiveChanged = true;
         };
       } else {
         ev = () => {
           isActive = !isActive;
+          isActiveChanged = true;
         };
       }
 
       document.getElementById("ohc-button").addEventListener("click", ev);
-
+    } else {
+      await delay();
       createButton();
     }
   };
 
-  const sortButtonClickEvent = () => {
+  const sortButtonClickEvent = async () => {
     const btns = document.querySelectorAll("#sort-menu a");
 
     if (btns.length) {
       for (const b of btns) {
         b.addEventListener("click", () => {
-          setTimeout(() => {
-            hangulCommentCnt = 0;
-          }, 3000);
+          if (isActive) {
+            if (isProMode) {
+              setTimeout(() => detect(true), 1000);
+              setTimeout(() => detect(true), 3000);
+              setTimeout(() => detect(true), 5000);
+            } else {
+              setTimeout(() => (hangulCommentCnt = 0), 1000);
+              setTimeout(() => (hangulCommentCnt = 0), 3000);
+              setTimeout(() => (hangulCommentCnt = 0), 5000);
+            }
+          }
         });
       }
       isAddedSortEvent = true;
+    } else {
+      await delay();
+      sortButtonClickEvent();
     }
   };
 
-  if (isProMode) {
-    // Pro Mode
+  // pro
+  const observer = new MutationObserver((mutations) => {
+    mutations.forEach(function (mutation) {
+      if (mutation.nextSibling?.nodeName === "#text" && mutation.addedNodes.length) {
+        for (const node of mutation.addedNodes) {
+          if (node.id === "content-text" && node.classList?.contains("ytd-comment-renderer")) {
+            const comment = node.innerText.substr(0, 6);
 
-    const observer = new MutationObserver((mutations) => {
-      mutations.forEach(function (mutation) {
-        if (mutation.nextSibling?.nodeName === "#text" && mutation.addedNodes.length) {
-          for (const node of mutation.addedNodes) {
-            if (node.id === "content-text" && node.classList?.contains("ytd-comment-renderer")) {
-              const comment = node.innerText.substr(0, 6);
+            let isHangulComment = false;
 
-              let isHangulComment = false;
-
-              for (const c of comment) {
-                if (isHangulText(c)) {
-                  isHangulComment = true;
-                  break;
-                }
+            for (const c of comment) {
+              if (isHangulText(c)) {
+                isHangulComment = true;
+                break;
               }
-
-              if (!isHangulComment) node.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = "none";
             }
+
+            if (!isHangulComment) node.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.style.display = "none";
           }
         }
-      });
+      }
     });
+  });
+  const observerConfig = { childList: true, subtree: true };
 
-    const config = { childList: true, subtree: true };
+  // normal
+  const detect = (isNoSlice) => {
+    const list = Array.from(document.querySelectorAll("#content-text.ytd-comment-renderer")).slice(isNoSlice ? 0 : hangulCommentCnt);
 
-    const watchInterval = setInterval(() => {
+    for (const c of list) {
+      const text = c.innerText.substr(0, 6);
+      let isHangulComment = false;
+
+      for (const t of text) {
+        if (isHangulText(t)) {
+          isHangulComment = true;
+          if (!isNoSlice) hangulCommentCnt++;
+          break;
+        }
+      }
+
+      if (!isHangulComment) c.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+    }
+  };
+
+  /****************************************************************************************** */
+
+  const watchInterval = setInterval(
+    () => {
       const newPathName = window.location.pathname + window.location.search;
+      const isNewPath = prevPathName !== newPathName;
+      const isWatch = newPathName.includes("watch");
 
-      if (newPathName !== pathName) {
-        isBtnCreated = false;
-        isAddedSortEvent = false;
+      if (isNewPath && isWatch) {
+        isActive = isAlwaysActive;
+        createButton();
+        sortButtonClickEvent();
+      }
 
-        if (newPathName.includes("watch")) {
-          if (!isBtnCreated) createButton();
-          if (!isAddedSortEvent) sortButtonClickEvent();
+      // pro
+      if (isProMode && isWatch && (isActiveChanged || isNewPath)) {
+        observer.disconnect();
 
+        if (isActive) {
           const target = document.getElementById("comments");
-
-          if (isActive && target) {
-            observer.disconnect();
-            observer.observe(target, config);
-            pathName = newPathName;
-          }
-        } else {
-          observer.disconnect();
-          pathName = newPathName;
-        }
-      }
-    }, 3000);
-  } else {
-    // Normal Mode
-
-    let isDetecting = false;
-
-    const detect = () => {
-      isDetecting = true;
-      const list = Array.from(document.querySelectorAll("#content-text.ytd-comment-renderer")).slice(hangulCommentCnt);
-
-      for (const c of list) {
-        const text = c.innerText.substr(0, 6);
-        let isHangulComment = false;
-
-        for (const t of text) {
-          if (isHangulText(t)) {
-            isHangulComment = true;
-            hangulCommentCnt++;
-            break;
-          }
+          observer.observe(target, observerConfig);
         }
 
-        if (!isHangulComment) c.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.remove();
+        if (isActiveChanged) isActiveChanged = false;
       }
-      isDetecting = false;
-    };
 
-    setInterval(() => {
-      const newPathName = window.location.pathname + window.location.search;
+      // normal
+      if (!isProMode && isActive && isWatch) {
+        detect();
+      }
 
-      if (newPathName !== pathName) {
-        isAddedSortEvent = false;
-        isBtnCreated = false;
-        hangulCommentCnt = 0;
-      }
-      if (newPathName.includes("watch") && !isDetecting) {
-        if (!isAddedSortEvent) sortButtonClickEvent();
-        if (!isBtnCreated) createButton();
-        if (isActive) detect();
-      }
-    }, 3000);
-  }
+      prevPathName = newPathName;
+    },
+    isProMode ? 2000 : 3000
+  );
 });
