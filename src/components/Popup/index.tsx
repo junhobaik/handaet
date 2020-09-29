991bd2cd259d5e1e384ce12d79242092f9cf57b3
Junho Baik <junhobaik@gmail.com>
2020-09-30 00:13:55 +0900

    Popup 구현
    - 마크업, 스타일 구현
    - 항상 켜기를 키고 끄는 기능 구현
    - 항상 켜기 값 크롬 로컬 스토리지에 연동 구현

--- src/components/Popup/index.tsx
+++ src/components/Popup/index.tsx
@@ -1,8 +1,59 @@
 import * as React from "react";
+import { useState, useEffect } from "react";
 import "./index.scss";
 
 const Popup = () => {
-  return <div id="popup">popup</div>;
+  const [isLoad, setIsLoad] = useState<boolean>(false);
+  const [isAlwaysOn, setIsAlwaysOn] = useState<boolean>(false);
+
+  const toggleAlwaysOn = () => {
+    if (isLoad) {
+      chrome.storage.local.set(
+        {
+          "handaet-ao": !isAlwaysOn ? "on" : "off",
+        },
+        () => {
+          setIsAlwaysOn((prev: boolean) => !prev);
+        }
+      );
+    }
+  };
+
+  useEffect(() => {
+    chrome.storage.local.get("handaet-ao", (items) => {
+      if (items["handaet-ao"]) setIsAlwaysOn(items["handaet-ao"] === "on");
+    });
+
+    setIsLoad(true);
+  }, []);
+
+  return (
+    <div id="popup">
+      <h1 className="title-text">한댓 | 설정</h1>
+      <div className="settings-wrap">
+        <div className={`setttings-inner-wrap ${isLoad ? "loaded" : "loading"}`}>
+          <div className="always-on-setting">
+            <span>항상 한글 댓글만 보기</span>
+            <div
+              className={`slide-btn-box ${isAlwaysOn ? "on" : "off"}`}
+              role="button"
+              onClick={() => toggleAlwaysOn()}
+            >
+              <div className="slide-btn-circle"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <footer>
+        <a href="mailto:junhobaik@gmail.com">
+          <span>© 2020 Junho Baik</span>
+        </a>
+        <a href="#">
+          <span>문제 제보</span>
+        </a>
+      </footer>
+    </div>
+  );
 };
 
 export default Popup;
